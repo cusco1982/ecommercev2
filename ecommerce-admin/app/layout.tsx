21dc43623f25707d537dd33d0f4de60008bef45a
Julian <cusco2891@gmail.com>
2023-12-06 23:59:15 -0500

    admin dashboard

--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+
+import { ModalProvider } from '@/providers/modal-provider'
+import { ToasterProvider } from '@/providers/toast-provider'
+
+import './globals.css'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
+// export const metadata = {
 export const metadata: Metadata = {
   title: 'Admin Dashboard',
   description: 'Admin Dashboard',
@@ -15,15 +21,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <ClerkProvider>
-
-
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+
+          <ToasterProvider />
+          <ModalProvider />
+          {children}
+
+
+        </body>
       </html>
-
-
     </ClerkProvider>
   )
-}
+};
