4b5bf40a8342570c32b1c3e881eee1601bb05c3f
Julian <cusco2891@gmail.com>
2023-12-18 21:44:17 -0500

    next/config to allow for images

--- ecommerce-store/components/ui/product-card.tsx
+++ ecommerce-store/components/ui/product-card.tsx
@@ -1,9 +1,30 @@
 'use client';
 
-const ProductCard = () => {
-  return (
-    <div>ProductCard</div>
-  )
+import Image from "next/image";
+
+import { Product } from "@/types";
+
+interface ProductCard {
+    data: Product;
+}
+
+const ProductCard: React.FC<ProductCard> = ({
+    data
+}) => {
+    return (
+        <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+            {/* Images and Actions */}
+            <div className="aspect-square rounded-xl bg-gray-100 relative">
+
+                <Image
+                    src={data?.images?.[0]?.url}
+                    alt="Image"
+                    fill
+                />
+
+            </div>
+        </div>
+    )
 };
 
 export default ProductCard;
