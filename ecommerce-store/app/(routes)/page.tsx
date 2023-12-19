05477468ab3c365074fe9cbe645aaa184a3a9d2e
Julian <cusco2891@gmail.com>
2023-12-18 20:34:16 -0500

    get product action

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,19 +1,21 @@
 import getBillboard from "@/actions/get-billboard";
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
+import getProducts from "@/actions/get-products";
 
 export const revalidate = 0;
 
 
 const HomePage = async () => {
 
+  const products = await getProducts({ isFeatured: true })
   const billboard = await getBillboard("a4269087-4e49-4f3a-8f35-5ac046e4e67a");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
 
-        <Billboard data={billboard}/>
+        <Billboard data={billboard} />
 
       </div>
     </Container>
