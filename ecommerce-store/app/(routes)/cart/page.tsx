074e2e6827b07f9a76e97458a02f68c1376154f4
Julian <cusco2891@gmail.com>
2023-12-20 03:46:33 -0500

    cart punctuation

--- ecommerce-store/app/(routes)/cart/page.tsx
+++ ecommerce-store/app/(routes)/cart/page.tsx
@@ -29,7 +29,7 @@ const CartPage = () => {
                     <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
                         <div className="lg:col-span-7">
                             {cart?.items.length === 0 && (
-                                <p className="text-neutral-500">Your cart is empty</p>
+                                <p className="text-neutral-500">Your cart is empty.</p>
                             )}
                             <ul>
                                 {cart?.items.map((item) => (
