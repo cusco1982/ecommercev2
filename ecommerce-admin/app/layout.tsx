4da7e2c83e490df128302b4fb038f6288a2f6b02
Julian <cusco2891@gmail.com>
2023-12-20 03:59:32 -0500

    themeprovider added but error warning

--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 
-import { ModalProvider } from '@/providers/modal-provider'
-import { ToasterProvider } from '@/providers/toast-provider'
+import { ModalProvider } from '@/providers/modal-provider';
+import { ToasterProvider } from '@/providers/toast-provider';
+import { ThemeProvider } from '@/providers/theme-provider';
 
-import './globals.css'
+import './globals.css';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -26,12 +27,13 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 
-          <ToasterProvider />
-          <ModalProvider />
-          {children}
+            <ToasterProvider />
+            <ModalProvider />
+            {children}
 
-
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
