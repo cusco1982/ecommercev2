61c9bb8dd017adf880c7d98a86a6a5bb1c155241
Julian <cusco2891@gmail.com>
2023-12-19 21:44:11 -0500

    summary component

---
+++ ecommerce-store/app/(routes)/cart/components/summary.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import axios from "axios";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { toast } from "react-hot-toast";
+
+import Button from "@/components/ui/button";
+import Currency from "@/components/ui/currency";
+import useCart from "@/hooks/use-cart";
+
+const Summary = () => {
+    return (
+        <div>Summary</div>
+    )
+};
+
+export default Summary;
