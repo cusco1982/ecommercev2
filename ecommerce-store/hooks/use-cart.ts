81031064d2f9c0227561859e81cc67ca7224e67d
Julian <cusco2891@gmail.com>
2023-12-19 20:49:32 -0500

    use-cart hook finished

--- ecommerce-store/hooks/use-cart.ts
+++ ecommerce-store/hooks/use-cart.ts
@@ -1,19 +1,39 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import toast from 'react-hot-toast';
 
 import { Product } from '@/types';
 
-interface PreviewModalStore {
-    isOpen: boolean;
-    data?: Product;
-    onOpen: (data: Product) => void;
-    onClose: () => void;
+interface CartStore {
+    items: Product[];
+    addItem: (data: Product) => void;
+    removeItem: (id: string) => void;
+    removeAll: () => void;
 };
 
-const usePreviewModal = create<PreviewModalStore>((set) => ({
-    isOpen: false,
-    data: undefined,
-    onOpen: (data: Product) => set({ data, isOpen: true }),
-    onClose: () => set({ isOpen: false }),
-}));
+const useCart = create(
+    persist<CartStore>((set, get) => ({
+        items: [],
+        addItem: (data: Product) => {
+            const currentItems = get().items;
+            const exisitingItem = currentItems.find((item) => item.id === data.id);
 
-export default usePreviewModal;
+            if (exisitingItem) {
+                return toast("Item already in cart.");
+            }
+
+            set({ items: [...get().items, data] });
+            toast.success("Item added to cart.");
+        },
+        removeItem: (id: string) => {
+            set({ items: [...get().items.filter((item) => item.id !== id)] });
+            toast.success("Item removed from cart.");
+        },
+        removeAll: () => set({ items: [] }),
+    }), {
+        name: 'cart-storage',
+        storage: createJSONStorage(() => localStorage),
+    })
+)
+
+export default useCart;
