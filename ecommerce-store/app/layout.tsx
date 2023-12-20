14eae553462c078dfd7b8a46d794b92d07a1af14
Julian <cusco2891@gmail.com>
2023-12-19 20:29:27 -0500

    toastprovider added to global layout

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
 import ModalProvider from '@/providers/modal-provider';
+import ToastProvider from '@/providers/toast-provider';
 
 import './globals.css'
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={font.className}>
 
         <ModalProvider />
+        <ToastProvider/>
         <Navbar />
         {children}
         <Footer />
